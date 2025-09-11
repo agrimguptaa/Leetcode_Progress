7fd947c01bc2be560e0139119401f85432e6ea6d
agrimguptaa <agrimvarshney0111@gmail.com>
2025-09-12 00:44:28 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (18.22%) - LeetSync

---
+++ 66-plus-one/plus-one.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] plusOne(int[] digits) {
+        return plus(digits);
+    }
+    public static int[]plus(int[]arr){
+		for(int i=arr.length-1;i>=0;i--) {
+			if(arr[i]<9) {
+				arr[i]++;
+				return arr;
+			}
+			arr[i]=0;
+		}
+		arr=new int[arr.length+1];
+		arr[0]=1;
+		return arr;
+	}
+}
