38f4e2a214a8c6686f7559bcbe88c745753e7da4
agrimguptaa <agrimvarshney0111@gmail.com>
2025-10-02 20:44:07 +0530

    Time: 0 ms (100.00%) | Memory: 40.8 MB (51.31%) - LeetSync

---
+++ 190-reverse-bits/reverse-bits.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int reverseBits(int n) {
+        int output=0;
+        for(int i=0;i<32;i++){
+            output<<=1;
+            output |= (n&1);
+            n>>=1;
+        }
+        return output;
+    }
+}
