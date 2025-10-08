b0f3423885dbdbb36df71c975d661c14eca86b78
agrimguptaa <agrimvarshney0111@gmail.com>
2025-10-08 15:42:17 +0530

    Time: 0 ms (100.00%) | Memory: 41.6 MB (44.07%) - LeetSync

---
+++ 678-valid-parenthesis-string/valid-parenthesis-string.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean checkValidString(String s) {
+        return hai(s);
+    }
+    public static boolean hai(String s) {
+        int low = 0; 
+        int high = 0;
+        for (char c : s.toCharArray()) {
+            if (c == '(') {
+                low++;
+                high++;
+            } else if (c == ')') {
+                if (low > 0) low--;
+                high--;
+            } else {
+                if (low > 0) low--;
+                high++;
+            }
+            if (high < 0) return false;
+        }
+        return low == 0;
+    }
+}
