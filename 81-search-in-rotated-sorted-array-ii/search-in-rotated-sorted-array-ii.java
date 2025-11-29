32223882ededf7d05aeff874381af36bb474c83e
agrimguptaa <agrimvarshney0111@gmail.com>
2025-11-29 12:51:36 +0530

    Time: 0 ms (100.00%) | Memory: 45.1 MB (7.39%) - LeetSync

---
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.java
@@ -0,0 +1,34 @@
+class Solution {
+    public boolean search(int[] arr, int target) {
+        int lo=0;
+        int hi=arr.length-1;
+        while(lo<=hi) {
+            int mid=lo+(hi-lo)/2;
+            if(arr[mid]==target) {
+                return true;
+            }
+            if(arr[lo]==arr[mid]&&arr[mid]==arr[hi]) {
+                lo++;
+                hi--;
+                continue;
+            }
+            if(arr[lo]<=arr[mid]) {
+                if(arr[lo]<=target && target<=arr[mid]) {
+                    hi=mid-1;
+                }
+                else {
+                    lo=mid+1;
+                }
+            }
+            else {
+                if(arr[mid]<=target && target<=arr[hi]) {
+                    lo=mid+1;
+                }
+                else {
+                    hi=mid-1;
+                }
+            }
+        }
+        return false;
+    }
+}
