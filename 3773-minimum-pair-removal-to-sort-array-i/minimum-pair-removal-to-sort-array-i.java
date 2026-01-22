048dbb56eb0156409490207b2cb2c923641ab6f6
agrimguptaa <agrimvarshney0111@gmail.com>
2026-01-22 22:22:27 +0530

    Time: 1 ms (100.00%) | Memory: 44.3 MB (84.20%) - LeetSync

---
+++ 3773-minimum-pair-removal-to-sort-array-i/minimum-pair-removal-to-sort-array-i.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int minimumPairRemoval(int[] nums) {
+        int ans = 0, n = nums.length;
+        while (!isSorted(nums, n)) {
+            ans += 1;
+            int minSum = Integer.MAX_VALUE, pos = -1;
+            for (int i = 1; i < n; i++) {
+                int sum = nums[i - 1] + nums[i];
+                if (sum < minSum) {
+                    minSum = sum;
+                    pos = i;
+                }
+            }
+            nums[pos - 1] = minSum;
+            for (int i = pos; i < n - 1; i++){
+                nums[i] = nums[i + 1];
+            }
+            n--;
+        }
+        return ans;
+    }
+
+    private boolean isSorted(int[] nums, int n) {
+        for (int i = 1; i < n; i++) {
+            if (nums[i] < nums[i - 1]) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
