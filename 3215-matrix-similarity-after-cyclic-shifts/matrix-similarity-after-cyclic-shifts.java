a44bcf739b27905cc6720fffe5a5c876675f5c8c
agrimguptaa <agrimvarshney0111@gmail.com>
2026-03-27 22:32:29 +0530

    Time: 1 ms (99.47%) | Memory: 46.9 MB (64.00%) - LeetSync

---
+++ 3215-matrix-similarity-after-cyclic-shifts/matrix-similarity-after-cyclic-shifts.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean areSimilar(int[][] mat, int k) {
+        int n = mat[0].length;
+        k = k % n;
+        for(int i=0;i<mat.length;i++){
+            for(int j=0;j<n;j++){
+                if (mat[i][j] != mat[i][(j + k) % n]) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
