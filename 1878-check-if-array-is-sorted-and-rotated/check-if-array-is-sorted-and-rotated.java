70fa51d138d93db77736b245759aeaaebfc0ee7c
agrimguptaa <agrimvarshney0111@gmail.com>
2026-05-23 20:31:35 +0530

    Time: 0 ms (100.00%) | Memory: 43 MB (44.94%) - LeetSync

---
+++ 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean check(int[] nums) {
+        int n = nums.length, dip = 0;
+        for (int i = 0; i < n; i++) {
+            if (nums[i] > nums[(i + 1) % n]) {
+                dip++;
+            }
+        }
+        return dip <= 1;
+    }
+}
