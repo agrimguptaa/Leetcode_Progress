bcefc4715a418d157179fb7ab9253f17060a5724
agrimguptaa <agrimvarshney0111@gmail.com>
2026-09-06 21:27:12 +0530

    Time: 28 ms (5.20%) | Memory: 58.2 MB (9.68%) - LeetSync

---
+++ 115-distinct-subsequences/distinct-subsequences.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int numDistinct(String s, String t) {
+        Integer[][] dp = new Integer[s.length()][t.length()];
+        return solve(s, t, 0, 0, dp);
+    }
+
+    private int solve(String s, String t, int i, int j, Integer[][] dp) {
+        if (j == t.length()) {
+            return 1;
+        }
+        if (i == s.length()) {
+            return 0;
+        }
+        if (dp[i][j] != null) {
+            return dp[i][j];
+        }
+        if (s.charAt(i) == t.charAt(j)) {
+            dp[i][j] = solve(s, t, i + 1, j, dp) + solve(s, t, i + 1, j + 1, dp);
+        } else {
+            dp[i][j] = solve(s, t, i + 1, j, dp);
+        }
+        return dp[i][j];
+    }
+}
